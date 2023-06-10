e51ac847f037741c110c94e36d9c408017fe1bd0
@hexfa <amir.dehdarian2009@gmail.com>
2023-06-10 13:07:08 +0430

    Set navigator , create app router , create recycler bin screen , implement drawer widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,48 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:todo_bloc/models/tasks_model.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:todo_bloc/services/app_router.dart';
 
 import '../screens/tasks_screen.dart';
 import 'blocs/bloc_exports.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  //* Setup Hydrated Storage
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory appDocDir = await getApplicationDocumentsDirectory();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: appDocDir,
+  );
+
+  log(appDocDir.toString());
+
+  runApp(
+    MyApp(
+      appRouter: AppRouter(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final AppRouter appRouter;
+  const MyApp({
+    super.key,
+    required this.appRouter,
+  });
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) =>
-          TasksBloc()..add(AddTask(task: TaskModel(title: 'title1'))),
+      create: (context) => TasksBloc(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.grey,
         ),
         home: const TasksScreen(),
+        onGenerateRoute: appRouter.onGenerateRoute,
       ),
     );
   }
